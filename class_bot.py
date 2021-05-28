c1e95f7278a665e4da95ee2cae99adf1edda30f0
Albert Fugardo <albertfugardo@gmail.com>
2021-05-28 16:32:21 +0200

    igo and bot with pep8 style

--- class_bot.py
+++ class_bot.py
@@ -10,7 +10,7 @@ from class_igo import *
 TOKEN_ALBERT = "1848938537:AAEImx4WFL91JFydr9FnfmUIHMuxw1YFJqY"
 TOKEN_MAURO = "1609114464:AAHK86rLORDYaxcjKw9gEOy0sw_IQ04i_oY"
 
-TOKEN = TOKEN_ALBERT #explicar al readme lo del token, que a l'entregar la pràctica això s'ha de treure
+TOKEN = TOKEN_ALBERT  # explicar al readme lo del token, que a l'entregar la pràctica això s'ha de treure
 
 PLACE = 'Barcelona, Catalonia'
 GRAPH_FILENAME = 'barcelona.graph'
@@ -19,18 +19,20 @@ HIGHWAYS_URL = 'https://opendata-ajuntament.barcelona.cat/data/dataset/1090983a-
 CONGESTIONS_URL = 'https://opendata-ajuntament.barcelona.cat/data/dataset/8319c2b1-4c21-4962-9acd-6db4c5ff1148/resource/2d456eb5-4ea6-4f68-9794-2f3f1a58a933/download'
 TRAFFIC_COLORS = ['blue', 'yellow', 'orange', 'red', 'purple', 'black', 'black']
 
-people = {} #it is a dictionary with key the id of the user, and attributes the latitude and longitude of its position
-positions = {} #dictionary with id as keys, every id containing a dictionary with the name of a place as id and its coordinates as attributes
+people = {}  # it is a dictionary with key the id of the user, and attributes the latitude and longitude of its position
+positions = {}  # dictionary with id as keys, every id containing a dictionary with the name of a place as id and its coordinates as attributes
+
 
 def is_number(s):
     """
-    It recieves a string and it returns true if it could be a float, and false otherwise.
+    It receives a string and it returns true if it could be a float, and false otherwise.
     """
     try:
         float(s)
         return True
     except ValueError:
         return False
+
 
 def read_arguments(context, update):
     """
@@ -44,12 +46,12 @@ def read_arguments(context, update):
     is_coordinates = True
     coordinates = []
     for symbol in context.args:
-        is_coordinates = is_number(symbol) #to check if the attributes are coordinates or the name of a place
+        is_coordinates = is_number(symbol)  # to check if the attributes are coordinates or the name of a place
         if(is_coordinates):
             coordinates.append(float(symbol))
         place = place + ' ' + symbol
     if is_coordinates and len(coordinates) == 2:
-        return coordinates[0], coordinates[1] #we have received coordinates
+        return coordinates[0], coordinates[1]  # we have received coordinates
     else:
         try:
             place = place + ' Barcelona'
@@ -63,19 +65,22 @@ def read_arguments(context, update):
                 chat_id=update.effective_chat.id,
                 text='The place indicated is not correct. Please, write it again and check that the address is correct.')
 
+
 def start(update, context):
     """It starts the chat with the user."""
     id = update.effective_chat.id
     fullname = update.effective_chat.first_name + ' ' + update.effective_chat.last_name
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi %s! I am the GuiderBot. Try the /help command to know more about me and my functions!" %(fullname))
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Hi %s! I am the GuiderBot. Try the /help command to know more about me and my functions!" % (fullname))
+
 
 def help(update, context):
     """It explains all the available functions and what they do."""
     bot_info = ["You can execute the following commands:"]
     bot_info.append("/start - Say hi to the bot, he will reply back!")
-    bot_info.append("/author - It will be displayed the name of the authors and a link to the GitHub repository.")
-    bot_info.append("/go destination - You will receive an optimal route to your destination calculated with iGo.")
-    bot_info.append("/where - You will receive a small map of your current location.")
+    bot_info.append("/author - The name of the authors and a link to the GitHub repository will be displayed.")
+    bot_info.append("/go destination - You will receive the optimal route, calculated with iGo, that goes from your position to the desired place.")
+    bot_info.append("/where - You will receive a small map with your current location on it.")
     bot_info.append("/set name place - Save a place you usually go to with a name (only 1 word). This way you can /go name !")
     bot_info.append("/myplaces - Sends a list of your saved places.")
 
@@ -107,7 +112,7 @@ def go(update, context):
         origin_lat = people[id][0]
         origin_lon = people[id][1]
 
-        if id in positions and context.args[0] in positions[id]: #the place the user wants to go is a 'set' place
+        if id in positions and context.args[0] in positions[id]:  # if the place the user wants to go is a 'set' place
             destination_lat = positions[id][context.args[0]][0]
             destination_lon = positions[id][context.args[0]][1]
         else:
@@ -116,14 +121,14 @@ def go(update, context):
         path, total_time = bcn_graph.get_shortest_path_with_ispeed(origin_lat, origin_lon, destination_lat, destination_lon)
 
         file = "%d.png" % random.randint(1000000, 9999999)
-        map = StaticMap(SIZE, SIZE) #map
-        path_size = len(path) #size
+        map = StaticMap(SIZE, SIZE)  # map
+        path_size = len(path)  # size
 
-        map.add_marker(IconMarker((path[0]['x'], path[0]['y']), 'marker.png', 16, 32)) #highlights the origin node
-        map.add_marker(IconMarker((path[-1]['x'], path[-1]['y']), 'flag.png', 0, 22)) #highlights the destination node
+        map.add_marker(IconMarker((path[0]['x'], path[0]['y']), 'marker.png', 16, 32))  # highlights the origin node
+        map.add_marker(IconMarker((path[-1]['x'], path[-1]['y']), 'flag.png', 0, 22))  # highlights the destination node
 
         i = 0
-        while i < path_size-1: #it draws the path on the map
+        while i < path_size-1:  # it draws the path on the map
             congestion = bcn_graph.digraph.edges[path[i]['node_id'], path[i+1]['node_id']]['congestion']
             map.add_line(Line(((path[i]['x'], path[i]['y']), (path[i+1]['x'], path[i+1]['y'])), TRAFFIC_COLORS[congestion], 3))
             i = i + 1
@@ -151,7 +156,7 @@ def where(update, context):
         lat = people[id][0]
         lon = people[id][1]
         file = "%d.png" % random.randint(1000000, 9999999)
-        mapa = StaticMap(SIZE, SIZE) #adjusts the size of the map
+        mapa = StaticMap(SIZE, SIZE)  # adjusts the size of the map
         mapa.add_marker(IconMarker((lon, lat), 'marker.png', 16, 32))
         image = mapa.render()
         image.save(file)
@@ -165,26 +170,32 @@ def where(update, context):
             text='💣')
         context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text='Share your current position or use /pos to be able to show the position on a map!.')
+            text='Share your current position to be able to show it on a map!')
 
 
 def pos(update, context):
     """It sets the current location of the user to the position given by him."""
-    id = update.effective_chat.id
-    if id in positions and context.args[0] in positions[id]: #if the place where the user is located is a 'set' placed
-        lat = positions[id][context.args[0]][0]
-        lon = positions[id][context.args[0]][1]
-    else:
-        lat, lon = read_arguments(context, update)
-    people[id] = (lat, lon)
+    try:
+        id = update.effective_chat.id
+        a = context.args[0]  # to make an error appear if no name has been given
+        if id in positions and context.args[0] in positions[id]:  # if the place where the user is located is a 'set' placed
+            lat = positions[id][context.args[0]][0]
+            lon = positions[id][context.args[0]][1]
+        else:
+            lat, lon = read_arguments(context, update)
+        people[id] = (lat, lon)
 
-    place = ""
-    for word in context.args:
-        place += " "
-        place += word
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text='You are now in' + place)
+        place = ""
+        for word in context.args:
+            place += " "
+            place += word
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='You are now in' + place)
+    except:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='Write the position where you want to be!')
 
 
 def set(update, context):
@@ -192,12 +203,12 @@ def set(update, context):
     key = context.args[0]
     context.args.remove(key)
     lat, lon = read_arguments(context, update)
-    if not id in positions:
-        positions[id] = {} #we declare positions[id] as a dictionary if it is empty
-    positions[id][key] = (lat,lon)
+    if id not in positions:
+        positions[id] = {}  # we declare positions[id] as a dictionary if it is empty
+    positions[id][key] = (lat, lon)
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Succesfully saved '" + key + "' as a location.")
+        text="Successfully saved '" + key + "' as a location.")
 
 
 def print_places(update, context):
@@ -210,7 +221,7 @@ def print_places(update, context):
             lat = key[1][0]
             lon = key[1][1]
             file = "%d.png" % random.randint(1000000, 9999999)
-            mapa = StaticMap(SIZE, SIZE) #adjusts the size of the map
+            mapa = StaticMap(SIZE, SIZE)  # adjusts the size of the map
             mapa.add_marker(IconMarker((lon, lat), 'house.png', 16, 32))
             image = mapa.render()
             image.save(file)
@@ -221,11 +232,11 @@ def print_places(update, context):
     else:
         context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text="There are no saved locations. You can save one with /set, for more info see /help.")
+            text="There are no saved locations. You can save one with /set. For more info see /help.")
 
 
 def current_position(update, context):
-    """this function is called every time we get a new location of a user"""
+    """This function is called every time we get a new location of a user."""
     id = update.effective_chat.id
     message = update.edited_message if update.edited_message else update.message
     lat, lon = message.location.latitude, message.location.longitude
@@ -248,7 +259,7 @@ dispatcher.add_handler(CommandHandler('myplaces', print_places))
 dispatcher.add_handler(MessageHandler(Filters.location, current_position))
 
 print("Starting the bot, please wait a few seconds...")
-bcn_graph = iGraph(PLACE, GRAPH_FILENAME, HIGHWAYS_URL, CONGESTIONS_URL) #posar-ho abans de les funcions
+bcn_graph = iGraph(PLACE, GRAPH_FILENAME, HIGHWAYS_URL, CONGESTIONS_URL)
 try:
     bcn_graph.get_traffic()
 except:
@@ -263,6 +274,6 @@ while True:
     time.sleep(300)
     try:
         bcn_graph.get_traffic()
-        print("Succesfully updated live traffic data") #hauríem de fer que també s'actualitzés el mapa, no?, o fer-ho cada minut lo del mapa
+        print("Successfully updated live traffic data.")
     except:
-        print("An error ocurred while getting live traffic data")
+        print("An error occurred while getting live traffic data.")
--- class_igo.py
+++ class_igo.py
@@ -11,7 +11,7 @@ import numpy as np
 import os
 import random
 
-Highway = collections.namedtuple('Highway', 'tram')  # Tram
+Highway = collections.namedtuple('Highway', 'tram')
 Congestion = collections.namedtuple('Congestion', 'tram')
 
 
@@ -47,17 +47,19 @@ class iGraph():
         networkx.set_edge_attributes(self.digraph, dict(values), name="bearing")
 
     def _add_attributes(self):
+        """
+        It adds all the necessary attributes to the edges.
+        """
         networkx.set_edge_attributes(self._graph, 0, "congestion")
         networkx.set_edge_attributes(self.digraph, 0, "congestion")
-        networkx.set_edge_attributes(self._graph, 0, "itime") # crec que no el necessita
+        networkx.set_edge_attributes(self._graph, 0, "itime")  # crec que no el necessita
         networkx.set_edge_attributes(self.digraph, 0, "itime")
-        networkx.set_edge_attributes(self._graph, 0, "bearing") # Edges without a bearing are laces
+        networkx.set_edge_attributes(self._graph, 0, "bearing")  # Edges without a bearing are laces
         networkx.set_edge_attributes(self.digraph, 0, "bearing")
         osmnx.bearing.add_edge_bearings(self._graph)
-        self._add_edge_bearings_digraph() # Osmnx bearing function is only for multigraphs
+        self._add_edge_bearings_digraph()  # Osmnx bearing function is only for multigraphs
 
-
-    def _add_highway(self, node1, node2): # Tram en angles? xD
+    def _add_highway(self, node1, node2):
         try:
             route1 = osmnx.shortest_path(self.digraph, node1, node2)
             route2 = osmnx.shortest_path(self.digraph, node2, node1)
@@ -75,7 +77,7 @@ class iGraph():
             else:
                 self._highways_nodes.append([])
             self._print_progress_bar(i+1, 534)
-        print() # New line after progress bar
+        print()  # New line after progress bar
 
     # Output methods:
     def __str__(self):
@@ -129,7 +131,7 @@ class iGraph():
 
     # Live data reading and processing methods:
     def _coordinates_transform(self, coordinates):
-        """transforms coordinates from a string into a vector
+        """It transforms coordinates from a string into a vector
         in which every position is a coordinate"""
         v = []
         s = ""
@@ -157,7 +159,7 @@ class iGraph():
             for line in reader:
                 way_id, description, coordinates = line
                 result[int(way_id)-1] = self._coordinates_transform(coordinates)
-                #les tres variables són strings
+                # les tres variables són strings
 
         return result
 
@@ -173,7 +175,7 @@ class iGraph():
                 while way_id[0][i] != '#':
                     i += 1
                 result[int(way_id[0][0:i]) - 1] = way_id[0]
-                #way_id és de tipus list de mida 1. way_id[0] és un string
+                # way_id és de tipus list de mida 1. way_id[0] és un string
         return result
 
     def _update_traffic_data(self, traffic_now, tram):
@@ -196,66 +198,68 @@ class iGraph():
                 self._update_traffic_data(traffic_now, self._highways_nodes[i])
 
     # Other methods:
-    """Receives the coordinates of a location and it returns its nearest node of the graph."""
+
     def from_location_to_node(self, lat, lon):
-        node = osmnx.distance.nearest_nodes(self.digraph, lon, lat) #lon = 2. i lat = 41.
+        """Receives the coordinates of a location and it returns its nearest node of the graph."""
+        node = osmnx.distance.nearest_nodes(self.digraph, lon, lat)  # lon = 2. i lat = 41.
         return node
 
-
     def itime(self):
+        """
+        It adds a new attribute to every edge. This attribute is 'itime' and it
+        is calculated using the speed, length and congestions on a street. It
+        contains the expected time to go from the start to the end of the street.
+        """
         for edge in self.digraph.edges():
-            #self.digraph.edges[edge]['itime'] = 2 # formula
-
             if 'maxspeed' in self.digraph.edges[edge]:
                 speed = self.digraph.edges[edge]['maxspeed']
-                if(isinstance(speed, list)):  # hi ha speeds que són llistes
-                        min = 1000000 #we have found that the majority of edges with lists are little streets with [20,30]. There are also major streets, that's why if speed > 50 we choose the greatest speed, because there are more probabilities of having a tram there and therefore its congestion
-                        for gg in speed: #all the speeds are integers
+                if(isinstance(speed, list)):  # there are some speed given in lists
+                        min = 1000000
+                        for gg in speed:
                             if int(gg) < min:
                                 min = int(gg)
-                        speed = min
+                        speed = min  # we choose the slowest speed on the list
             else:
-                speed = 20 #posar una velocitat predeterminada pels carrers que no en tenen al graf
+                speed = 20  # predeterminated spped
 
-            if self.digraph.edges[edge]['congestion'] == 6: #the street is closed
+            if self.digraph.edges[edge]['congestion'] == 6:  # the street is closed
                 self.digraph.edges[edge]['itime'] = 100000
             else:
                 self.digraph.edges[edge]['itime'] = self.digraph.edges[edge]['length']*3.6/int(speed)
                 self.digraph.edges[edge]['itime'] *= (1+self.digraph.edges[edge]['congestion']/5)**2
                 self.digraph.edges[edge]['itime'] += 10
-                #print(self.digraph.edges[edge]['itime'])
 
-
-    """
-    Given origin and destination coordinates, it calculates the shortest path
-    between them taking into account the 'itime' attribute. It also calculates
-    the time needed to complete the path, and it returns the path with
-    information related to each node(such as its coordinates) and the total time.
-    """
     def get_shortest_path_with_ispeed(self, origin_lat, origin_lon, destination_lat, destination_lon):
+        """
+        Given origin and destination coordinates, it calculates the shortest path
+        between them taking into account the 'itime' attribute. It also calculates
+        the time needed to complete the path, and it returns the path with
+        information related to each node(such as its coordinates) and the total time.
+        """
         self.itime()
         origin_node = self.from_location_to_node(origin_lat, origin_lon)
         destination_node = self.from_location_to_node(destination_lat, destination_lon)
-        path = osmnx.distance.shortest_path(self.digraph, origin_node, destination_node, weight = 'itime')
+        path = osmnx.distance.shortest_path(self.digraph, origin_node, destination_node, weight='itime')
 
-        #now in path we have the 'number' of the nodes, but we want its coordinates, so we take each node attribute
         total_time = 0
         path_with_coordinates = []
         last_node = -1
+
+        # now in path we have the 'number' of the nodes, but we want its coordinates, so we take each node attribute
         for node in path:
             path_with_coordinates.append(self.digraph.nodes[node])
             path_with_coordinates[-1]['node_id'] = node
             if last_node != -1:
                 total_time = total_time + self.digraph.edges[last_node, node]["itime"]
             last_node = node
-        return path_with_coordinates, total_time #this will return a list of lists of the nodes constituting the shortest path between each origin-destination pair. If a path cannot be solved, this will return None for that path
+        return path_with_coordinates, total_time
 
-    """
-    Given a path that goes from one node to another, it calculates the minimum
-    region in which the path is visible. Then, it prints the map of the city
-    centered in the region, with the path visible on it.
-    """
-    def plot_path(self, path): #també es poden fer dues funcions, una que doni el mapa centrat i l'altre que doni el mapa complet
+    def plot_path(self, path):
+        """
+        Given a path that goes from one node to another, it calculates the minimum
+        region in which the path is visible. Then, it prints the map of the city
+        centered in the region, with the path visible on it.
+        """
         max_x = 0
         min_x = 1000
         max_y = 0
@@ -269,13 +273,13 @@ class iGraph():
                 min_y = node['y']
             elif node['y'] > max_y:
                 max_y = node['y']
-        bbox = (max_y + 0.005, min_y - 0.005, max_x + 0.005, min_x - 0.005) #+-0.005 to be able to see every node/edge completely
+        bbox = (max_y + 0.005, min_y - 0.005, max_x + 0.005, min_x - 0.005)  # +-0.005 to be able to see every node/edge completely
 
         route = []
         for node in path:
             route.append(node['osmid'])
-        osmnx.plot_graph_route(self._graph, route, route_color = 'r', route_linewidth = 3, route_alpha = 1, node_size = 0, bgcolor='k', bbox = bbox) #route_alpha és la opacitat, close = True perquè sinó el codi no avança
-
+        osmnx.plot_graph_route(self._graph, route, route_color='r', route_linewidth=3,
+                               route_alpha=1, node_size=0, bgcolor='k', bbox=bbox)
 
 
 # Data
@@ -286,10 +290,9 @@ HIGHWAYS_URL = 'https://opendata-ajuntament.barcelona.cat/data/dataset/1090983a-
 CONGESTIONS_URL = 'https://opendata-ajuntament.barcelona.cat/data/dataset/8319c2b1-4c21-4962-9acd-6db4c5ff1148/resource/2d456eb5-4ea6-4f68-9794-2f3f1a58a933/download'
 
 
-
 # Testing code
-#bcn_map = iGraph(PLACE, GRAPH_FILENAME, HIGHWAYS_URL, CONGESTIONS_URL)
-#bcn_map.itime()
+# bcn_map = iGraph(PLACE, GRAPH_FILENAME, HIGHWAYS_URL, CONGESTIONS_URL)
+# bcn_map.itime()
 """
 bcn_map.print_bearings()
 location1 = osmnx.geocoder.geocode("Camp Nou Barcelona")
@@ -299,8 +302,8 @@ bcn_map.plot_path(path)
 """
 
 
-#bcn_map.print_graph()
+# bcn_map.print_graph()
 
 # print(bcn_map)
-# bcn_map.get_traffic()  # Actualitzar dades de tràfic
-# bcn_map.print_congestions()  # Plot amb el tràfic
+# bcn_map.get_traffic()  # Actualitzar dades de trànsit
+# bcn_map.print_congestions()  # Plot amb el trànsit
